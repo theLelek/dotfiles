6d5a32c8a42aa1e3d31e043c801527a1fe3a80e4
lelek <heschl.leon@gmail.com>
2026-08-05 23:39:13 +0000

    switched esc and capslock in hyprland - added starship to .bashrc

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -220,7 +220,7 @@ hl.config({
         kb_layout  = "de",
         kb_variant = "",
         kb_model   = "",
-        kb_options = "",
+        kb_options = "caps:swapescape",
         kb_rules   = "",
 
         follow_mouse = 1,
